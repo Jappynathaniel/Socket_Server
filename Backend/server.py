28bb2b1aee59c4c32b7ba818a071a7b3131ad394
Jasper Nathaniel J <95154891+Jappynathaniel@users.noreply.github.com>
2023-06-22 23:28:17 +0530

    Update server.py

--- Backend/server.py
+++ Backend/server.py
@@ -1,28 +1,43 @@
-import socket 
-import subprocess 
-import os 
-host = ''
-port =8877 
+import socket
+import subprocess
 
-s = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
-s.bind((host,port)) 
-s.listen() 
-conn , addr = s.accept()
-print("{} connected to port {}".format(addr[0],addr[1])) 
-while True: 
-    data = conn.recv(1024)
-    data.strip()
-    if not data : 
-        break 
-    else: 
-        
-        data = data.decode()
-        print("echo > {}".format(data)) 
-        if (data == "quit"): 
-            break 
-        else: 
-            proc = subprocess.Popen(data , stdout=subprocess.PIPE , shell=True , stderr=subprocess.STDOUT )
-            (out,err) = proc.communicate()
-            data = data+out.decode
-            conn.sendall(data.encode())
-s.close()
+host = ''
+port = 8877
+
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+try:
+    s.bind((host, port))
+    s.listen()
+    print("Server started. Listening on port", port)
+
+    conn, addr = s.accept()
+    print("{} connected to port {}".format(addr[0], addr[1]))
+
+    while True:
+        data = conn.recv(1024).decode().strip()
+        if not data:
+            break
+
+        print("Received command: {}".format(data))
+
+        if data == "quit":
+            break
+
+        try:
+            proc = subprocess.Popen(data, stdout=subprocess.PIPE, shell=True, stderr=subprocess.STDOUT)
+            (out, err) = proc.communicate()
+            response = out.decode()
+        except subprocess.CalledProcessError as e:
+            response = str(e.output)
+
+        conn.sendall(response.encode())
+
+except socket.error as e:
+    print("Socket error:", str(e))
+finally:
+    if conn:
+        conn.close()
+    s.close()
+
+print("Server closed.")
